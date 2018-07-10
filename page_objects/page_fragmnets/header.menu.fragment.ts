937eff0bf5b4de69e01d0947cd00c06b419779d5
mholosiuk <mhosoliuk@gmail.com>
2018-07-10 19:18:35 +0300

    Added data to constructor

--- page_objects/page_fragmnets/header.menu.fragment.ts
+++ page_objects/page_fragmnets/header.menu.fragment.ts
@@ -14,6 +14,15 @@ class HeaderMenu {
 
   constructor() {
     this.homePage = $('.logo')
+    this.main = $('a.sel')
+    this.forum = $('.b-head>ul>li:nth-child(3)>a')
+    this.strip = $('.b-head>ul>li:nth-child(4)>a')
+    this.salary = $('.b-head>ul>li:nth-child(5)>a')
+    this.job = $('.b-head>ul>li:nth-child(6)>a')
+    this.calendar = $('.m-last')
+    this.telegram = $('.m-hide.t-hide.tg')
+    this.search = $('.search')
+    this.loginReg = $('#login-link')
   }
 
   public goToHomePage() {
