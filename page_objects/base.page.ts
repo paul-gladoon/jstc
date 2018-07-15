712f6dbeffcc5d2a0f6e3fb0fec8a671fb88675e
Paul G <paul.gladoon@gmail.com>
2018-07-15 12:18:40 +0300

    change project to Jenkins

--- page_objects/base.page.ts
+++ page_objects/base.page.ts
@@ -2,15 +2,15 @@ import { ElementFinder, $, browser, ExpectedConditions as EC } from 'protractor'
 
 class BasePage {
 
- private homePageBtn: ElementFinder
+ private logo: ElementFinder
 
  constructor() {
-   this.homePageBtn = $('.logo')
+   this.logo = $('.navbar-brand')
  }
 
  public async start() {
    await browser.get('/')
-   await browser.wait(EC.visibilityOf(this.homePageBtn), 5000, `Waiting for ${this.homePageBtn}`)
+   await browser.wait(EC.visibilityOf(this.logo), 5000, `Waiting for ${this.logo}`)
  }
 }
 
