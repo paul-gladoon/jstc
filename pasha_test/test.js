656301c08332f45bb42a0fff6031aa9d77791359
Paul G <paul.gladoon@gmail.com>
2019-02-24 22:26:09 +0200

    complete_first_dz

---
+++ pasha_test/test.js
@@ -0,0 +1,14 @@
+const name = 'Pavel '
+let age = 29
+const agePlus = 10
+const greeting = ' Hello'
+const myGreeting = name + age + greeting
+let info = name + age
+
+// #1
+console.log(myGreeting)
+
+// #2
+console.log(info)
+info = name + (age + agePlus)
+console.log(info)
