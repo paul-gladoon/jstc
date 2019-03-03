c244cd8953c03eb39a35a9917cc15dd385360241
Dmitry Gladun <47780493+4gusreopen@users.noreply.github.com>
2019-03-03 22:34:52 +0200

    Test3 branch (#18)
    * lesson_2
    
    * thirdLesson

---
+++ dima_test/lesson_2/test_2.js
@@ -0,0 +1,18 @@
+const num = 1 // Number 
+const str = "Hello" // String
+const varNull = null // Null
+const varUndef = undefined // Undefinded  
+const varTrue = true // Boolean
+const varFalse = false // Boolean
+const varObj = {} // Object
+
+const varArray = [] // Array/Object
+const varFunc = function() {} // Function/Object
+const varNaN = NaN // Not a number 
+
+
+const a = 1 === 1 // Strict compare 
+const a = 1 == 1 // Non-strict compare 
+const a = 1 !== 1 // Not equal 
+// <,>,<=,>=
+
