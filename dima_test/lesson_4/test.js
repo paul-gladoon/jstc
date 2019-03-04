41f09ed81b1dd639d00feeaa39d242ac73513c1c
4gusgame <4gusgame@gmail.com>
2019-03-04 20:39:23 +0200

    4lesson

---
+++ dima_test/lesson_4/test.js
@@ -0,0 +1,68 @@
+// function greeting() {
+//   console.log('HELLO')
+// }
+
+// greeting()
+
+
+// function test() {
+//   const a = 5
+//   const b = 6
+//   const c = a * b
+//   console.log(c)
+// }
+
+// test()
+
+// function greeting(name) {
+//     console.log('Hello ' + name)
+// }
+
+// greeting('Dima')
+
+
+   // TASKS //
+   // #1 // 
+
+  //  function yourName(name) {
+  //     return name
+  //  }
+  //   yourName('Dima')
+
+  //  function yourAge(age) {
+  //     return age
+  //  }
+  //   yourAge('20')
+
+  //  function yourSex(sex) {
+  //     return sex
+  //  }
+  //   yourSex('Man')
+
+  //    console.log(yourName('Dima') + ' ' + yourAge('20') + ' ' + yourSex('Man'))
+
+
+  // #2 //
+
+  // function taskTwo(name,age) {
+  //   if (name === 'Admin', age >= 18) {
+  //     message = 'Hello my GOD'
+  //   }
+  //   else if (name === 'Valera', age < 18) {
+  //     message = 'Hello noob'
+  //   }
+  //   else if (name === 'Pasha') {
+  //     message = 'Who are you?'
+  //   }
+  // }
+  //    taskTwo('Pasha')
+  //    console.log(message)
+
+
+  // #3 //
+
+  // function task3(number) {
+  // return Math.round(task3(number))
+  // }
+  //   task3(1.3)
+  //   console.log(Math.round(task3(number)))
