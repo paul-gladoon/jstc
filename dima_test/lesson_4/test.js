b5a36ba614feec1fde44a039e15ba69679065b10
4gusgame <4gusgame@gmail.com>
2019-03-06 23:05:21 +0200

    changes

--- dima_test/lesson_4/test.js
+++ dima_test/lesson_4/test.js
@@ -1,8 +1,8 @@
-// function greeting() {
+// function hello() {
 //   console.log('HELLO')
 // }
 
-// greeting()
+// hello()
 
 
 // function test() {
@@ -21,48 +21,79 @@
 // greeting('Dima')
 
 
-   // TASKS //
-   // #1 // 
+//   //  TASKS //
+//   //  #1 
 
-  //  function yourName(name) {
-  //     return name
-  //  }
-  //   yourName('Dima')
+//    function yourName(name) {
+//       return name
+//    }
+    
 
-  //  function yourAge(age) {
-  //     return age
-  //  }
-  //   yourAge('20')
+//    function yourAge(age) {
+//       return age
+//    }
+    
 
-  //  function yourSex(sex) {
-  //     return sex
-  //  }
-  //   yourSex('Man')
+//    function yourSex(sex) {
+//       return sex
+//    }
+    
+//    function result(name, age, sex) {
+//       console.log(yourName(name) + ' ' + yourAge(age) + ' ' + yourSex(sex))
+//    }
 
-  //    console.log(yourName('Dima') + ' ' + yourAge('20') + ' ' + yourSex('Man'))
+//    result('Dima', '20', 'Man')
+  
+   // #2 //
 
-
-  // #2 //
-
-  // function taskTwo(name,age) {
-  //   if (name === 'Admin', age >= 18) {
-  //     message = 'Hello my GOD'
+  // function taskTwo(name, age) {
+  //   if (name === 'Admin' && age >= 18) {
+  //    return 'Hello my GOD'
   //   }
-  //   else if (name === 'Valera', age < 18) {
-  //     message = 'Hello noob'
+  //   else if (name === 'Valera' && age < 18) {
+  //     return 'Hello noob'
   //   }
-  //   else if (name === 'Pasha') {
-  //     message = 'Who are you?'
+  //   else if (name === 'Pasha' && age === undefined) {
+  //     return 'Who are you?'
   //   }
   // }
-  //    taskTwo('Pasha')
-  //    console.log(message)
-
+  
+  // console.log(taskTwo('Valera', 17))    
 
   // #3 //
 
   // function task3(number) {
-  // return Math.round(task3(number))
+  //   return Math.round(number)
   // }
-  //   task3(1.3)
-  //   console.log(Math.round(task3(number)))
+  //  console.log(task3(1.3))
+
+  // #4 //
+
+  // function first(numOne, numTwo) {
+  //   return numOne + numTwo
+  // }
+  // function second(numAny) {
+  //   return numAny * 10
+  // }
+  // function third(numOne, numTwo, numAny) {
+  //   return first(numOne, numTwo) + second(numAny)
+  // }
+  // third(5, 7, 4)
+
+  // #5 //
+
+  // function first(name, sex, age) {
+  //   return name + ' ' + sex + ' ' + age
+  // }
+
+  // function second(name, sex, age) {
+  //   return first(name, sex, age) + ' ' + 'Hello'
+  // }
+  // console.log(second('Dima','Man', 20))
+
+  // #6 //
+
+  // function math(num, degree) {
+  //   return Math.pow(num, degree)
+  // }
+  // console.log(math(5, 3))
