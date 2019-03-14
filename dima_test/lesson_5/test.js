3d5ffe1e655fc997151028c601d2a3ce81df2f24
4gusgame <4gusgame@gmail.com>
2019-03-14 14:26:49 +0200

    test5

---
+++ dima_test/lesson_5/test.js
@@ -0,0 +1,28 @@
+// #1 // 
+
+// let arr1 = [1, NaN, 3.14, 'What']
+
+// function test1(arr) {
+//    console.log(arr.join(''))
+// }
+// test1(arr1)
+
+// #2 // 
+
+// let arr4 = [1, 2, 3, 4, 5, 6]
+
+// function test3(arr, inElem) {
+//   arr.splice(inElem, 1)
+//   console.log(arr4)
+// }
+// test3(arr4, 2)
+
+// #3 // 
+
+// let arr2 = [1, 2, 3, 4]
+// let arr3 = [5, 6, 7, 8]
+
+// function test2(mas1, mas2) {
+//   console.log(arr4 = mas1.concat(mas2))
+// }
+// test2(arr2,arr3)
