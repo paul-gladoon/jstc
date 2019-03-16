b4a5ac79045b64c425426159c7c24706bf0759af
Dmitry Gladun <47780493+4gusreopen@users.noreply.github.com>
2019-03-16 23:00:17 +0200

    test5 (#20)
    * test5
    
    * Changes

---
+++ dima_test/lesson_5/test.js
@@ -0,0 +1,26 @@
+// #1 // 
+
+const arr1 = ['1', 'NaN', 'Wa', 'What']
+
+function test1(arr) {
+ return arr.join(' ')
+}
+
+// #2 // 
+
+const arr4 = [1, 2, 3, 4, 5, 6]
+
+function test3(arr, inElem) {
+  arr.splice(inElem, 1)
+}
+test3(arr4, 2)
+
+// #3 // 
+
+let arr2 = [1, 2, 3, 4]
+let arr3 = [5, 6, 7, 8]
+
+function test2(mas1, mas2) {
+  return mas1.concat(mas2)
+}
+const arr4 = test2(arr2,arr3) 
